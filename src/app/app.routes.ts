998324597370b4c3c521f42cceb31afb9ace8b54
Manas Malla <manasmalla.dev@gmail.com>
2024-07-09 23:09:29 +0530

    added bg for text
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TermsComponent } from './terms/terms.component';
+import { FaqComponent } from './faq/faq.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: HomeComponent,
+    },
+    {
+     path: 'about',
+     component: AboutComponent,   
+    },
+    {
+        path: "terms",
+        component: TermsComponent,
+    },
+    {
+        path: 'faq',
+        component: FaqComponent,
+    }
+];
